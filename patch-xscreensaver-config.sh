56d34c7d7e2dcb20d70f16a840975d986b62db47
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-02-14 11:58:42 -0600

    Having trouble getting patches to work reliably.

--- patch-xscreensaver-config.sh
+++ patch-xscreensaver-config.sh
@@ -5,8 +5,13 @@ script_dir="$(dirname "$0")"
 patch_file="$script_dir/timetunnel-logos.patch"
 
 if [ -e "$patch_file" ]; then
-	patch --unified --forward "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch"
-	exit 0
+	if patch --unified --forward --fuzz "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch";
+	then
+		exit 0
+	else
+		echo "Maybe patch ~/.xscreensaver manually?"
+		exit 1
+	fi
 else
 	echo "Error: cannot find patch file at: $patch_file"
 	echo "Maybe patch ~/.xscreensaver manually?"

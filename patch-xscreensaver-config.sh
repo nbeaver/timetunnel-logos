161b732c82fad281239280d726f45f388110b9a0
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2015-02-13 23:12:13 -0600

    Update patch script for new patch.

--- patch-xscreensaver-config.sh
+++ patch-xscreensaver-config.sh
@@ -5,7 +5,7 @@ script_dir="$(dirname "$0")"
 patch_file="$script_dir/timetunnel-logos.patch"
 
 if [ -e "$patch_file" ]; then
-	patch --normal --forward "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch"
+	patch --unified --forward "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch"
 	exit 0
 else
 	echo "Error: cannot find patch file at: $patch_file"

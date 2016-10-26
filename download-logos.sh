4d5f22eb1b5dc5eac7ccd80f9e791512cb5d2350
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 21:43:15 -0500

    Use braces for consistency.

--- download-logos.sh
+++ download-logos.sh
@@ -9,7 +9,7 @@ fi
 # Check if any of the images are missing.
 if test -f "${INSTALL}/tardis.xpm" && test -f "${INSTALL}/whohead1.xpm" && test -f "${INSTALL}/whologo.xpm"
 then
-    echo "Already installed images in $INSTALL"
+    echo "Already installed images in ${INSTALL}"
 else
     wget --output-document="/tmp/timetunnel-4.22.tar.gz" "${URL}"
     tar --ungzip --extract --file "/tmp/timetunnel-4.22.tar.gz" --directory "/tmp/"

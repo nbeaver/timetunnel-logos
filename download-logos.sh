2e55d57b2b09f807089ab51a0005c17e1afdcee7
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 21:42:11 -0500

    Use spaces for indentation.

--- download-logos.sh
+++ download-logos.sh
@@ -9,11 +9,11 @@ fi
 # Check if any of the images are missing.
 if test -f "${INSTALL}/tardis.xpm" && test -f "${INSTALL}/whohead1.xpm" && test -f "${INSTALL}/whologo.xpm"
 then
-	echo "Already installed images in $INSTALL"
+    echo "Already installed images in $INSTALL"
 else
-	wget --output-document="/tmp/timetunnel-4.22.tar.gz" "${URL}"
-	tar --ungzip --extract --file "/tmp/timetunnel-4.22.tar.gz" --directory "/tmp/"
-	mkdir --parents "${INSTALL}"
-	cp '/tmp/xscreensaver-4.22/hacks/images/'{tardis.xpm,whohead1.xpm,whologo.xpm} "${INSTALL}"
-	echo "Copied images to ${INSTALL}"
+    wget --output-document="/tmp/timetunnel-4.22.tar.gz" "${URL}"
+    tar --ungzip --extract --file "/tmp/timetunnel-4.22.tar.gz" --directory "/tmp/"
+    mkdir --parents "${INSTALL}"
+    cp '/tmp/xscreensaver-4.22/hacks/images/'{tardis.xpm,whohead1.xpm,whologo.xpm} "${INSTALL}"
+    echo "Copied images to ${INSTALL}"
 fi

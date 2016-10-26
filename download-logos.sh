a7a73a16197a1b818f354b918004e4d4c994fac8
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 21:51:18 -0500

    Start error handling.

--- download-logos.sh
+++ download-logos.sh
@@ -9,12 +9,22 @@ fi
 # Check if any of the images are missing.
 if test -f "${INSTALL}/tardis.xpm" && test -f "${INSTALL}/whohead1.xpm" && test -f "${INSTALL}/whologo.xpm"
 then
-    echo "Already installed images in ${INSTALL}"
+    printf "Error: already installed images in ${INSTALL}\n" >&2
     exit 1
 fi
 
-wget --output-document="/tmp/timetunnel-4.22.tar.gz" "${URL}"
+if ! wget --output-document="/tmp/timetunnel-4.22.tar.gz" "${URL}"
+then
+    printf "Error: could not download from URL: ${URL}\n" >&2
+    exit 1
+fi
+
 tar --ungzip --extract --file "/tmp/timetunnel-4.22.tar.gz" --directory "/tmp/"
-mkdir --parents "${INSTALL}"
+if ! test -d "${INSTALL}"
+then
+    printf "Error: install directory does not exist: ${INSTALL}\n" >&2
+    exit 1
+fi
+
 cp '/tmp/xscreensaver-4.22/hacks/images/'{tardis.xpm,whohead1.xpm,whologo.xpm} "${INSTALL}"
 echo "Copied images to ${INSTALL}"

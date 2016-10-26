fd2c774cf12622da16877a74f240c1aafad952c4
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 22:48:03 -0500

    Use spaces for indentation.

--- patch-xscreensaver-config.sh
+++ patch-xscreensaver-config.sh
@@ -5,15 +5,15 @@ script_dir="$(dirname "$0")"
 patch_file="$script_dir/timetunnel-logos.patch"
 
 if [ -e "$patch_file" ]; then
-	if patch --unified --forward --fuzz "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch";
-	then
-		exit 0
-	else
-		echo "Maybe patch ~/.xscreensaver manually?"
-		exit 1
-	fi
+    if patch --unified --forward --fuzz "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch";
+    then
+        exit 0
+    else
+        echo "Maybe patch ~/.xscreensaver manually?"
+        exit 1
+    fi
 else
-	echo "Error: cannot find patch file at: $patch_file"
-	echo "Maybe patch ~/.xscreensaver manually?"
-	exit 1
+    echo "Error: cannot find patch file at: $patch_file"
+    echo "Maybe patch ~/.xscreensaver manually?"
+    exit 1
 fi

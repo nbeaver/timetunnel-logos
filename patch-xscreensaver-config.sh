9d496511f05d105fb63b4b70a1f335f85e6fdab6
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 23:26:14 -0500

    Start trying to convert the patching script to not need any external files.

--- patch-xscreensaver-config.sh
+++ patch-xscreensaver-config.sh
@@ -1,14 +1,22 @@
 #! /usr/bin/env bash
 
-script_dir="$(dirname "$0")"
-
-patch_file="$script_dir/timetunnel-logos.patch"
-
-if ! test -f "$patch_file"
+if test $# -ne 1
 then
-    echo "Error: cannot find patch file at: $patch_file"
-    echo "Maybe patch ~/.xscreensaver manually?"
+    printf "Usage: %s %s\n" "$0" "${HOME}/.xscreensaver" >&2
     exit 1
 fi
 
-patch --unified --forward --fuzz "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch"
+CONFIG_FILE="$1"
+
+patch "${CONFIG_FILE}" << EOF
+227c227,232
+< - GL: 				timetunnel -root			    \n\
+---
+>   GL: 				timetunnel -root -tardis		      \
+> 				  $HOME/.local/share/icons/xscreensaver/timetunnel/tardis.xpm   \
+> 				  -head					      \
+> 				  $HOME/.local/share/icons/xscreensaver/timetunnel/whohead1.xpm   \
+> 				  -marquee				      \
+> 				  $HOME/.local/share/icons/xscreensaver/timetunnel/whologo.xpm \n\
+
+EOF

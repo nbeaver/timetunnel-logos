259e6990a7e4588f5b80effe2cb1ecee9fe7070e
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 22:49:48 -0500

    Stop using nested conditionals.

--- patch-xscreensaver-config.sh
+++ patch-xscreensaver-config.sh
@@ -4,16 +4,11 @@ script_dir="$(dirname "$0")"
 
 patch_file="$script_dir/timetunnel-logos.patch"
 
-if [ -e "$patch_file" ]; then
-    if patch --unified --forward --fuzz "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch";
-    then
-        exit 0
-    else
-        echo "Maybe patch ~/.xscreensaver manually?"
-        exit 1
-    fi
-else
+if ! test -f "$patch_file"
+then
     echo "Error: cannot find patch file at: $patch_file"
     echo "Maybe patch ~/.xscreensaver manually?"
     exit 1
 fi
+
+patch --unified --forward --fuzz "$HOME/.xscreensaver" "$script_dir/timetunnel-logos.patch";

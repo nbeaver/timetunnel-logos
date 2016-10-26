ceffea2aaca488f32c27b0d0938bbc8852ae3d28
Nathaniel Beaver <nathanielmbeaver@gmail.com>
2016-10-25 21:26:02 -0500

    Somewhat more readable.

--- download-logos.sh
+++ download-logos.sh
@@ -1,17 +1,19 @@
 #! /usr/bin/env bash
 
+URL="http://www.zettix.com/Graphics/timetunnel/timetunnel-4.22.tar.gz"
 INSTALL="$HOME/.local/share/icons/xscreensaver/timetunnel"
 if ! test -d "${INSTALL}"
 then
     mkdir "${INSTALL}"
 fi
 # Check if any of the images are missing.
-if [[ -f "$INSTALL/tardis.xpm" && -f "$INSTALL/whohead1.xpm" && -f "$INSTALL/whologo.xpm" ]]; then
+if test -f "${INSTALL}/tardis.xpm" && test -f "${INSTALL}/whohead1.xpm" && test -f "${INSTALL}/whologo.xpm"
+then
 	echo "Already installed images in $INSTALL"
 else
-	wget --output-document="/tmp/timetunnel-4.22.tar.gz" "http://www.zettix.com/Graphics/timetunnel/timetunnel-4.22.tar.gz"
+	wget --output-document="/tmp/timetunnel-4.22.tar.gz" "${URL}"
 	tar --ungzip --extract --file "/tmp/timetunnel-4.22.tar.gz" --directory "/tmp/"
-	mkdir --parents "$INSTALL"
-	cp '/tmp/xscreensaver-4.22/hacks/images/'{tardis.xpm,whohead1.xpm,whologo.xpm} "$INSTALL"
-	echo "Copied images to $INSTALL"
+	mkdir --parents "${INSTALL}"
+	cp '/tmp/xscreensaver-4.22/hacks/images/'{tardis.xpm,whohead1.xpm,whologo.xpm} "${INSTALL}"
+	echo "Copied images to ${INSTALL}"
 fi

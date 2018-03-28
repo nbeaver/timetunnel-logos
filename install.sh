c0906ddf3604cfd36bc2ca7f6b362ce7e4046d93
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-03-28 12:21:35 -0500

    Add single script as per https://github.com/nbeaver/timetunnel-logos/issues/1

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /usr/bin/env bash
+
+wget --no-verbose --no-clobber --recursive --level=1 --no-parent --no-directories \
+  --accept xpm \
+  -P ~/.local/share/icons/xscreensaver/timetunnel \
+  'http://www.zettix.com/Graphics/timetunnel/xscreensaver-4.22/hacks/images/'
+
+patch ~/.xscreensaver << EOF
+227c227,232
+< - GL: 				timetunnel -root			    \n\\
+---
+>   GL: 				timetunnel -root -tardis		      \\
+> 				  \$HOME/.local/share/icons/xscreensaver/timetunnel/tardis.xpm   \\
+> 				  -head					      \\
+> 				  \$HOME/.local/share/icons/xscreensaver/timetunnel/whohead1.xpm   \\
+> 				  -marquee				      \\
+> 				  \$HOME/.local/share/icons/xscreensaver/timetunnel/whologo.xpm \n\\
+EOF

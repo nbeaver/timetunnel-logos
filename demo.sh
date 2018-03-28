d5d9be1234465e68316a58a50943c85925ee99aa
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2018-03-28 10:26:54 -0500

    Script to run demonstration.

---
+++ demo.sh
@@ -0,0 +1,2 @@
+#! /usr/bin/env sh
+/usr/lib/xscreensaver/timetunnel -tardis ~/.local/share/icons/xscreensaver/timetunnel/tardis.xpm -head ~/.local/share/icons/xscreensaver/timetunnel/whohead1.xpm -marquee ~/.local/share/icons/xscreensaver/timetunnel/whologo.xpm
